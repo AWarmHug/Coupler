4092b02bdd788f5746cf86c7e38d6d2e8974d87c
renbin <renbin@chebada.com>
2022-03-07 19:15:02 +0800

    针对特定颜色修改TextColor

--- app/src/main/java/com/bingo/spadedemo/ui/MainFragment.kt
+++ app/src/main/java/com/bingo/spadedemo/ui/MainFragment.kt
@@ -40,7 +40,7 @@ class MainFragment : Fragment() {
         }
 
         view.findViewById<TextView>(R.id.bt2).setOnClickListener {
-            val newSkin = Skin.LIGHT
+            val newSkin =skin.value?:Skin.LIGHT
             val viewTheme=
                 ViewTheme(TextColor(textColor = Color.BLACK), background = Background(url = "https://www.baidu.com/img/pc_9c5c85e6b953f1d172e1ed6821618b91.png"))
             newSkin.themes["bdeed52e7398c38977691652f3670007"] = viewTheme
